b2ce28faff0143f852ebd54027b4e7198e240c35
Mayank <mk4664348@gmail.com>
2023-05-04 20:48:52 +0530

    Devloping back-end for login

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userModel = require('./models/User');
+const jwt = require('jsonwebtoken');
 //require('dotenv').config()
 
-dotenv.config()
 
-const mongoUrl = "";
+dotenv.config()
+//console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI);
+const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 
@@ -12,8 +17,13 @@ app.get('/test', (req, res) => {
     res.json('Test ok');
 })
 
-app.post('/register', (req, res)=>{
-
+app.post('/register', async(req, res)=>{
+    const {username, password} = req.body;
+    const createdUser = await User.create({username, password}); //Using await with promises
+    jwt.sign({userId:createdUser, _id}, jwtSecret, (err, token) => {
+        if (err) throw(err)
+        res.cookie('token', token).status(201).json('ok');
+    }) //Second approach to deal with promise
 })
 
 app.listen(4000, ()=>{
