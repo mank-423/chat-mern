46f7def2f13631b4c8d0da26a0440615409ceab5
Mayank <mk4664348@gmail.com>
2023-05-05 14:04:26 +0530

    Chat section devlopment

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import RegisterAndLoginForm from './pages/RegisterAndLoginForm'
 import { UserContext } from './UserContext';
+import Chat from './pages/Chat';
 
 export default function Routes() {
 
   const {username, id} = useContext(UserContext);
 
   if (username){
-    return "Logged in! " + username;
+    return <Chat />
   }
 
   return (
