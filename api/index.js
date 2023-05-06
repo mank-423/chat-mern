f27180e12c883e622681b0c8ff30c7d8c1e40183
Mayank <mk4664348@gmail.com>
2023-05-07 01:03:19 +0530

    Working on chat section

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs')
+const ws = require('ws');
 //require('dotenv').config()
 
 dotenv.config()
@@ -43,7 +44,7 @@ app.get('/profile', (req, res) => {
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const foundUser = await User.findOne({ username });
-    
+
     if (foundUser) {
         const passOk = bcrypt.compareSync(password, foundUser.password);
         if (passOk) {
@@ -82,6 +83,50 @@ app.post('/register', async (req, res) => {
 
 });
 
-app.listen(4040, () => {
+const server = app.listen(4040, () => {
     console.log("Listening on port 4040");
+});
+
+const wss = new ws.WebSocketServer({ server });
+wss.on('connection', (connection, req) => {
+    
+    //Read username and id from the cookie for this connection
+    const cookies = req.headers.cookie;
+
+    if (cookies) {
+        const tokenCookieString = cookies.split(';').find(str => str.startsWith('token='));
+
+        if (tokenCookieString) {
+            const token = tokenCookieString.split('=')[1];
+
+            if (token){
+                jwt.verify(token, jwtSecret, {}, (err, userData) => {
+                    if (err) throw err;
+                    const {userId, username} = userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                })
+            }
+        }
+    }
+
+    connection.on('message', (message)=>{
+        const messageData = JSON.parse(message.toString());
+        
+        const {recepient, text} = messageData;
+
+        if (recepient && text){
+            [...wss.client]
+                .filter(c => c.userId === recepient)
+                .forEach(c=> c.send(JSON.stringify({text})))
+        }
+    });
+
+    //Notify everyone about online people (when someone connects)
+    //[...wss.clients].map(c => ({userId:c.userId, username: c.username}))
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify({
+            online: [...wss.clients].map(c => ({userId:c.userId, username: c.username})),
+        }))
+    })
 });
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,31 +1,119 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import Avatar from '../components/Avatar';
+import Logo from '../components/Logo';
+import { UserContext } from '../UserContext.jsx';
 
 export default function Chat() {
+
+    const [ws, setWs] = useState(null);
+    const [onlinePeople, setOnlinePeople] = useState({});
+    const [selectedUserId, setSelectedUserID] = useState(null);
+    const { username, id } = useContext(UserContext);
+    const [newMessageText, setNewMessageText] = useState('');
+    const [messages, setMessages] = useState([]); 
+
+    function handleMessage(e) {
+        const messageData = JSON.parse(e.data);
+        if ('online' in messageData) {
+            showOnlinePeople(messageData.online)
+        }
+        else{
+            console.log({messageData});
+        }
+    }
+
+    function sendMessage(e) {
+        e.preventDefault();
+        ws.send(JSON.stringify({
+            message: {
+                recepient: selectedUserId,
+                text: newMessageText,
+            }
+        }));
+
+        setNewMessageText('');
+        setMessages(prev => ([...prev, {text: newMessageText, isOur: true}]))
+    }
+
+    function showOnlinePeople(peopleArray) {
+        const people = {};
+        peopleArray.forEach(({ userId, username }) => {
+            people[userId] = username;
+        });
+        setOnlinePeople(people);
+        console.log(people);
+    }
+
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:4040');
+        setWs(ws);
+        ws.addEventListener('message', handleMessage);
+    }, []);
+
+    const onlinePeopleExclOurUser = { ...onlinePeople };
+    delete onlinePeopleExclOurUser[id];
+
+
     return (
         <div className='flex h-screen'>
-
             <div className="bg-white w-1/3">
-                Contacts
+
+                <Logo />
+
+                {Object.keys(onlinePeopleExclOurUser).map(userId => (
+                    <div
+                        key={userId}
+                        onClick={() => { setSelectedUserID(userId); console.log(userId) }}
+                        className={'border-b border-gray-100 flex items-center gap-2 cursor-pointer ' + (userId === selectedUserId ? 'bg-blue-50' : '')}>
+                        {userId === selectedUserId && (
+                            <div className='w-1 bg-blue-500 h-12 rounded-r-md'> </div>
+                        )}
+
+                        <div className='flex gap-2 py-2 pl-4 items-center'>
+                            <Avatar userId={userId} username={onlinePeople[userId]} />
+                            <span>{onlinePeople[userId]}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
 
             <div className=" flex flex-col bg-blue-50 w-2/3 p-2">
                 <div className='flex-grow'>
-                    Messages with selected person
+                    {!selectedUserId && (
+                        <div className='flex items-center justify-center h-full text-gray-400'>&larr; Select a person from sidebar</div>
+                    )}
+
+                    {!!selectedUserId && (
+                        <div>
+                            {messages.map(message => (
+                                <div>
+                                    {message.text}
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
-                
-                <div className='flex gap-2'>
+
+                {!!selectedUserId && (
+                    <form className='flex gap-2' onSubmit={sendMessage}>
                         <input
+                            value={newMessageText}
+                            onChange={(e) => {
+                                setNewMessageText(e.target.value);
+                            }}
                             type="text"
                             placeholder='Type your message here'
                             className="bg-white border p-2 flex-grow rounded-sm"
                         />
 
                         <button className='bg-blue-500 p-2 text-white rounded-sm'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                             </svg>
                         </button>
-                </div>
+                    </form>
+                )}
+
             </div>
 
         </div>
